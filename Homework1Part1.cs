fff4af9dffd0eff89e3ad13d83f58c0311a9420f
allyh626 <ashernandez3@buffs.wtamu.edu>
2025-01-24 14:47:15 -0600

    Homework1

---
+++ Homework1Part1.cs
@@ -0,0 +1,15 @@
+﻿namespace Homework1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double X= 2.5;
+        double Y= 3.3;
+        double z = 34.9;
+        z =4*X*X+3*Y;
+        Console.WriteLine("X = 2.5, Y = 3.3");
+        Console.WriteLine("given  that Z = 4X^2 + 3Y");
+        Console.Write("The value of Z is: " + z + ".");
+    }
+}
